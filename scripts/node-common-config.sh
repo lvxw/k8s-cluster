cd879b581446e40cddac9a4746fc3ae64d76d17a
unknown <lvxuewen@youxin.com>
2019-11-06 12:01:48 +0800

    no message

--- scripts/node-common-config.sh
+++ scripts/node-common-config.sh
@@ -25,18 +25,7 @@ function addHosts(){
 }
 
 function commonConfigure(){
-echo "------------------------commonConfigure----------------------------"
-cat > /etc/docker/daemon.json <<EOF
-{
-    "exec-opts": ["native.cgroupdriver=systemd"],
-    "log-driver": "json-file",
-    "log-opts": {
-    "max-size": "100m"
-    }
-}
-EOF
-
-    systemctl daemon-reload
+    echo "------------------------commonConfigure----------------------------"
     systemctl restart docker
     systemctl enable docker
 
